8be3fed5abfe8479668f7795aa58f6734eae29c5
melnikov <melnikov161@gmail.com>
2018-10-23 19:25:50 +0500

    [FIX] Scroll

--- app/scripts/_scroll.js
+++ app/scripts/_scroll.js
@@ -1,15 +1,44 @@
-// import {$} from 'jquery'
-//
-// import 'smartscroll'
-// import EventEmitter from './_EventEmitter.min'
-//
-// let ee = new EventEmitter()
-//
-// $.smartscroll({
-//   autoHash: false,
-//   sectionScroll: true,
-//   sectionWrapperSelector: '.main',
-//   sectionClass: 'slide',
-//   eventEmitter: ee,
-//   bindSwipe: true
-// })
+/* global $ */
+
+/* global EventEmitter */
+let ee = new EventEmitter()
+
+$(document).ready(() => {
+  var resizing = false
+
+  scrollInit()
+  $(window).on('resize', function () {
+    if (!resizing) {
+      (!window.requestAnimationFrame)
+        ? setTimeout(scrollInit, 300)
+        : window.requestAnimationFrame(scrollInit)
+      resizing = true
+    }
+  })
+
+  function checkMQ () {
+    var dw = $('.page').outerWidth()
+
+    if (dw >= 1200) {
+      return 'desktop'
+    } else {
+      return 'mobile'
+    }
+  }
+
+  function scrollInit () {
+    var mq = checkMQ()
+
+    if (mq !== 'mobile') {
+      $.smartscroll({
+        autoHash: false,
+        sectionScroll: true,
+        sectionWrapperSelector: '.main',
+        sectionClass: 'slide',
+        eventEmitter: ee,
+        bindSwipe: true
+      })
+    }
+    resizing = false
+  }
+})
